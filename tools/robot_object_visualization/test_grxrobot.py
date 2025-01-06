5cdf0401c9c39161d057991054b5f5477573e1a6
yangqj <840155193@qq.com>
2025-01-06 14:44:57 +0800

    Replace the hand URDF model from jaw to fourier_hand_6dof.

--- tools/robot_object_visualization/test_grxrobot.py
+++ tools/robot_object_visualization/test_grxrobot.py
@@ -30,15 +30,17 @@ def demo(fix_root_link, balance_passive_force):
     loader.load_multiple_collisions_from_file = True
 
     robot: sapien.Articulation = loader.load(
-        "ManiSkill2_real2sim/mani_skill2_real2sim/assets/descriptions/grx_description/GR1T2/urdf/GR1T2_fourier_hand_6dof.urdf"
+        "ManiSkill2_real2sim/mani_skill2_real2sim/assets/descriptions/grx_description/GR1T2/urdf/GR1T2_fourier_hand_6dof_no_leg.urdf"
     )
     # robot: sapien.Articulation = loader.load("ManiSkill2_real2sim/mani_skill2_real2sim/assets/descriptions/googlerobot_description/google_robot_meta_sim_fix_wheel_fix_fingertip_recolor_cabinet_visual_matching_1.urdf")
     print(robot.get_links())
     robot.set_root_pose(sapien.Pose([0, 0, 0.98], [1, 0, 0, 0]))
     # Set initial joint positions
     qpos = [ 
-        # 0, 0, 0, -math.pi/2, 0, 0, 0, 1,
-        0.,0, 0, -math.pi/2, 0, 0, 0, -1,
+        0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        # 0, 0, 0, 0, 0, 0, 0, 
+        # 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
     ]
     robot.set_qpos(qpos)
     print(robot.get_qpos())
